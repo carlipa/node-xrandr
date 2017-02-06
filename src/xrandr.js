6421c12f5999875dc8fcadf135d73d1e3c82ca8f
Lionel Penaud <lpenaud@carlipa.com>
2017-02-06 14:49:07 +0100

    feat(rotation): add rotation

--- src/xrandr.js
+++ src/xrandr.js
@@ -1,6 +1,9 @@
 const CONNECTED_REGEX = /^(\S+) connected (?:(\d+)x(\d+))?/;
 const DISCONNECTED_REGEX = /^(\S+) disconnected/;
 const MODE_REGEX = /^\s+(\d+)x([0-9i]+)\s+((?:\d+\.)?\d+)([* ]?)([+ ]?)/;
+const ROTATION_LEFT = /^([^(]+) left \((?:(\d+)x(\d+))?/;
+const ROTATION_RIGHT = /^([^(]+) right \((?:(\d+)x(\d+))?/;
+const ROTATION_INVERTED = /^([^(]+) inverted \((?:(\d+)x(\d+))?/;
 
 function xrandrParser(input) {
   let strInput = input;
@@ -18,12 +21,21 @@ function xrandrParser(input) {
       parts = CONNECTED_REGEX.exec(line);
       result[parts[1]] = {
         connected: true,
-        modes: []
+        modes: [],
+        rotation: 'normal'
       };
       if (parts[2] && parts[3]) {
         result[parts[1]].width = parseInt(parts[2], 10);
         result[parts[1]].height = parseInt(parts[3], 10);
       }
+      if (ROTATION_LEFT.test(line)) {
+        result[parts[1]].rotation = 'left';
+      } else if (ROTATION_RIGHT.test(line)) {
+        result[parts[1]].rotation = 'right';
+      } else if (ROTATION_INVERTED.test(line)) {
+        result[parts[1]].rotation = 'inverted';
+      }
+
       lastInterface = parts[1];
     } else if (DISCONNECTED_REGEX.test(line)) {
       parts = DISCONNECTED_REGEX.exec(line);
--- test/index.spec.js
+++ test/index.spec.js
@@ -7,8 +7,16 @@ describe('parser', () => {
   const buffer1 = fs.readFileSync(`${__dirname}/fixtures/input1.txt`);
   const buffer2 = fs.readFileSync(`${__dirname}/fixtures/input1.txt`);
   const buffer3 = fs.readFileSync(`${__dirname}/fixtures/input3.txt`);
+  const buffer4 = fs.readFileSync(`${__dirname}/fixtures/input4.txt`);
+  const buffer5 = fs.readFileSync(`${__dirname}/fixtures/input5.txt`);
+  const buffer6 = fs.readFileSync(`${__dirname}/fixtures/input6.txt`);
+  const buffer7 = fs.readFileSync(`${__dirname}/fixtures/input7.txt`);
   const expected1 = JSON.parse(fs.readFileSync(`${__dirname}/fixtures/output1.json`).toString());
   const expected3 = JSON.parse(fs.readFileSync(`${__dirname}/fixtures/output3.json`).toString());
+  const expected4 = JSON.parse(fs.readFileSync(`${__dirname}/fixtures/output4.json`).toString());
+  const expected5 = JSON.parse(fs.readFileSync(`${__dirname}/fixtures/output5.json`).toString());
+  const expected6 = JSON.parse(fs.readFileSync(`${__dirname}/fixtures/output6.json`).toString());
+  const expected7 = JSON.parse(fs.readFileSync(`${__dirname}/fixtures/output7.json`).toString());
 
   it('should throw if no string is passed', () => {
     let value;
@@ -53,5 +61,25 @@ describe('parser', () => {
   it('should properly parse the file input3.txt', () => {
     const value = parser(buffer3.toString());
     expect(value).toEqual(expected3);
+  });
+
+  it('should properly parse the file input4.txt', () => {
+    const value = parser(buffer4.toString());
+    expect(value).toEqual(expected4);
+  });
+
+  it('should properly parse the file input5.txt', () => {
+    const value = parser(buffer5.toString());
+    expect(value).toEqual(expected5);
+  });
+
+  it('should properly parse the file input6.txt', () => {
+    const value = parser(buffer6.toString());
+    expect(value).toEqual(expected6);
+  });
+
+  it('should properly parse the file input7.txt', () => {
+    const value = parser(buffer7.toString());
+    expect(value).toEqual(expected7);
   });
 });
